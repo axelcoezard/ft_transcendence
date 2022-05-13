3f2a314c255d7053e1ea7bdf6993f1475beb3a61
mboy <melboy29@gmail.com>
2022-05-13 10:26:08 +0200

    Revert "Scenery update"
    This reverts commit 0760181857225b70df4e2ecec0a9f680b896b13d.

--- apps/front/src/components/Scenery/Illustration.tsx
+++ apps/front/src/components/Scenery/Illustration.tsx
@@ -1,16 +1,12 @@
 import * as React from "react";
+import FrameWaves from "./Frame.waves";
 import BackgroundWaves from "./Background.waves";
-import BackgroundBlobs from "./Background.blobs";
-import Asteroides from "./Asteroides";
-import Confetties from "./Confetties";
 import styles from '../../styles/Illustration.module.scss'
 
 function Illustration() {
   return <div className={styles.scenery}>
 		<BackgroundWaves />
-		<BackgroundBlobs />
-		<Asteroides />
-		<Confetties />
+		<FrameWaves />
   	</div>
 }
 
