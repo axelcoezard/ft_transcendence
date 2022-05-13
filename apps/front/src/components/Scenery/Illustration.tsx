0760181857225b70df4e2ecec0a9f680b896b13d
mboy <melboy29@gmail.com>
2022-05-13 09:21:24 +0200

    Scenery update

--- apps/front/src/components/Scenery/Illustration.tsx
+++ apps/front/src/components/Scenery/Illustration.tsx
@@ -1,12 +1,16 @@
 import * as React from "react";
-import FrameWaves from "./Frame.waves";
 import BackgroundWaves from "./Background.waves";
+import BackgroundBlobs from "./Background.blobs";
+import Asteroides from "./Asteroides";
+import Confetties from "./Confetties";
 import styles from '../../styles/Illustration.module.scss'
 
 function Illustration() {
   return <div className={styles.scenery}>
 		<BackgroundWaves />
-		<FrameWaves />
+		<BackgroundBlobs />
+		<Asteroides />
+		<Confetties />
   	</div>
 }
 
