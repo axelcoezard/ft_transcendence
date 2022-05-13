186653a1eb02e04cb250263fc6c2d33346ba6517
mboy <melboy29@gmail.com>
2022-05-13 16:42:37 +0200

    Scenery Done

--- apps/front/src/components/Scenery/Illustration.tsx
+++ apps/front/src/components/Scenery/Illustration.tsx
@@ -1,13 +1,25 @@
 import * as React from "react";
-import FrameWaves from "./Frame.waves";
 import BackgroundWaves from "./Background.waves";
+import BackgroundBlobs from "./Background.blobs";
+import Asteroides from "./Asteroides";
+import Confetties from "./Confetties";
+import ShootingStars from "./Shooting.stars";
+import Planets from "./Planets";
+import FrameWaves from "./Frame.waves";
+import Rocket from "./Rocket";
 import styles from '../../styles/Illustration.module.scss'
 
 function Illustration() {
-  return <div className={styles.scenery}>
+  return <g className={styles.scenery}>
 		<BackgroundWaves />
+		<BackgroundBlobs />
+		<Asteroides />
+		<Confetties />
+		<Planets />
+		<ShootingStars />
+		<Rocket />
 		<FrameWaves />
-  	</div>
+  	</g>
 }
 
 export default Illustration;
