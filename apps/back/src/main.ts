e8920319d4c68505e6fc9dd2ac96e1458bb98f2d
Axel Coézard <axel.coezard@gmail.com>
2022-05-13 13:11:36 +0200

    fix everything mboy removed from my work 🤡

--- apps/back/src/main.ts
+++ apps/back/src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import AppModule from './app.module';
-import Tchat from './tchat';
 
 (async () => {
-	const app = await NestFactory.create(AppModule, { cors: true });
-
-	const tchat = new Tchat(app);
-
-	await app.listen(3030);
+	(await NestFactory.create(AppModule, {
+		cors: true
+	})).listen(3030);
 })();
