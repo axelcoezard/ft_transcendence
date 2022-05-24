3a43085275598d5b605f180df0cff3785c077638
Axel Coézard <axel.coezard@gmail.com>
2022-05-24 10:02:39 +0200

    fiximports

--- apps/back/src/app.gateway.ts
+++ apps/back/src/app.gateway.ts
@@ -1,10 +1,8 @@
-import { Module, Logger, Inject } from '@nestjs/common';
+import { Logger, Inject } from '@nestjs/common';
 import { SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
-import { channel } from 'diagnostics_channel';
 import { Socket, Server } from 'socket.io';
 import ChannelBuilder from './builder/channel.builder';
 import MessageBuilder from './builder/message.builder';
-import Message from './entities/message.entity';
 import AppService from './services/app.service';
 
 @WebSocketGateway({
@@ -36,22 +34,23 @@ export class AppGateway
 		this.logger.log(`Client connected: ${client.id}`);
 	}
 
+	async sendTchatInformations(client: Socket, slug: string) {
+		client.emit("channel_set_list", await this.service.channels.getAll());
+		client.emit("channel_set_msg", await this.service.messages.getByChannel(slug));
+	}
+
 	@SubscribeMessage("channel_join")
 	async onJoinChannel(client: Socket, info) {
 		console.log("joined channel")
 		if (await this.service.channels.getBySlug(info.channel_slug))
-		{
-			client.emit("channel_set_list", await this.service.channels.getAll());
-			client.emit("channel_set_msg", await this.service.messages.getByChannel(info.channel_slug));
-			return;
-		}
+			return this.sendTchatInformations(client, info.channel_slug);
 
 		console.error(`Channel ${info.channel_slug} does not exist: creating...`)
-		await this.service.channels.create(
+		this.service.channels.create(
 			ChannelBuilder.new()
 			.setCreator(info.sender_id)
 			.setSlug(info.channel_slug)
-		)
+		).then(res => this.sendTchatInformations(client, info.channel_slug));
 	}
 
 	@SubscribeMessage("channel_msg")
