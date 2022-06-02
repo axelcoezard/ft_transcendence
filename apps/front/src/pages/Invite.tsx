4901cbe3f01233c57adcf8c681fdd72aecaa8f70
Melody Boy <mboy@c2r2p4.42nice.fr>
2022-06-02 11:43:11 +0200

    Victories & defeats

--- apps/front/src/pages/Invite.tsx
+++ apps/front/src/pages/Invite.tsx
@@ -10,6 +10,7 @@ const Invite = () => {
 
 	return <div className={styles.invite}>
 		<h1 className={styles.invite_h1}>INVITE</h1>
+		<p>Défis un joueur à une partie de Pong</p>
 		<div className={styles.invite_content}>
 			<input className={styles.invite_input}
 				type="text"
