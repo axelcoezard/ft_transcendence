0017f8747551077e17c54cba31c1a760fe6a52e0
Axel Coézard <axel.coezard@gmail.com>
2022-06-03 01:29:28 +0200

    little fix

--- apps/front/src/pages/Settings.tsx
+++ apps/front/src/pages/Settings.tsx
@@ -2,22 +2,7 @@ import QrCode from "../components/QrCode";
 import QrCodeValidator from "../components/QrCodeValidator";
 
 const Settings = () => {
-	return <div style={{
-		display: "flex",
-		flexDirection: "column",
-		alignItems: "center",
-		justifyContent: "center",
-		position: "absolute",
-		top: "50%",
-		left: "50%",
-		transform: "translate(-50%, -50%)",
-		width: "500px",
-		height: "400px",
-		zIndex: "1000",
-	}}>
-		<h1>Settings</h1>
-		<QrCode size={200} />
-		<QrCodeValidator />
+	return <div>
 	</div>
 }
 
