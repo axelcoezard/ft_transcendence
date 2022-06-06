5d474a2c70d00c66ae686ea3ae9c608ff0752440
Axel Coézard <axel.coezard@gmail.com>
2022-06-06 17:21:52 +0200

    fix invite resp

--- apps/front/src/pages/Invite.tsx
+++ apps/front/src/pages/Invite.tsx
@@ -8,24 +8,26 @@ const Invite = () => {
 		e.preventDefault();
 	}
 
-	return <div className={styles.invite}>
-		<h1 className={styles.invite_h1}>DUEL</h1>
-		<p>Défis un joueur à une partie de Pong</p>
-		<div className={styles.invite_content}>
+	return <section className={styles.invite}>
+		<div className={styles.invite_header}>
+			<h1>INVITE</h1>
+			<p>Rejoignez la partie d'un ami qui vous defie</p>
+		</div>
+		<form className={styles.invite_form}>
 			<input className={styles.invite_input}
 				type="text"
 				value={value}
-				placeholder="Enter private invite code"
+				placeholder="Code d'invitation"
 				onChange={(e: any) => {
 					setValue(e.currentTarget.value || e.target.value)
 			}}/>
 			<button className={styles.invite_button}
 				onClick={handleSubmit}
 				onSubmit={handleSubmit}>
-				<p className={styles.invite_text}>JOIN</p>
+				<p className={styles.invite_text}>Rejoindre</p>
 			</button>
-		</div>
-	</div>
+		</form>
+	</section>
 }
 
 export default Invite;
