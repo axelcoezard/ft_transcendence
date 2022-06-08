f7a8db05e43054dfb19da067aa33e5e61f34a564
Axel Coézard <axel.coezard@gmail.com>
2022-06-08 15:25:31 +0200

    removed logs from jwt strategy

--- apps/back/src/modules/auth/jwt.strategy.ts
+++ apps/back/src/modules/auth/jwt.strategy.ts
@@ -13,7 +13,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-	console.log("validation")
     return { id: payload.sub, username: payload.username };
   }
 }
